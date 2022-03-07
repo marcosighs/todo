e05fd22da0912ca6f45df14275497c4ce414232a
Marcos Lopes <94773225+marcosighs@users.noreply.github.com>
2022-03-07 19:02:58 -0300

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import "./Todo.css";
+import List from './List'
+import TodoForm from './TodoForm'
+import Item from './Item'
+
+function Todo() {
+  const [items, setItems] = useState([]);
+
+  function onAddItem(text) {
+
+    let item = new Item(text);
+
+    setItems([...items, item])
+  }
+
+  return (<div className="container">
+      <h1>Todo</h1>
+    <TodoForm onAddItem={onAddItem}></TodoForm>
+    <List items={items}></List>
+
+    </div>);
+}
+
+
+export default Todo;
